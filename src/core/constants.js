65b4d4ec25581f12d594c361e081042c903e02b7
ciordan <Catalin.Iordan@endava.com>
2020-10-18 21:41:50 +0300

    #42: Refactor

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,21 +1,23 @@
-export const HTML_EDITOR_TITLE = 'HTML';
-export const CSS_EDITOR_TITLE = 'CSS';
-export const JS_EDITOR_TITLE = 'JS';
+export const HTML_CONFIG = {
+  editorTitle: 'HTML',
+  language: 'html',
+  sample: `<div class='twenty-one-pilots'>Twenty One Pilots</div>`,
+};
 
-export const HTML_SAMPLE = `
-<div class='twenty-one-pilots'>Twenty One Pilots</div>
-`;
+export const CSS_CONFIG = {
+  editorTitle: 'CSS',
+  language: 'css',
+  sample: `.twenty-one-pilots {
+    color: red;
+}`,
+};
 
-export const CSS_SAMPLE = `
-.twenty-one-pilots {
-  color: red;
-}
-`;
-
-export const JS_SAMPLE = `
-  function sayHello() {
+export const JS_CONFIG = {
+  editorTitle: 'JS',
+  language: 'javascript',
+  sample: `function sayHello() {
     console.log('Hello world');
-  }
+}
 
-  sayHello();
-`;
+sayHello();`,
+};
--- src/features/code/ICode.js
+++ src/features/code/ICode.js
@@ -1,13 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
-import {
-  CSS_EDITOR_TITLE,
-  HTML_EDITOR_TITLE,
-  JS_EDITOR_TITLE,
-  HTML_SAMPLE,
-  CSS_SAMPLE,
-  JS_SAMPLE,
-} from '../../core/constants';
+import { CSS_CONFIG, HTML_CONFIG, JS_CONFIG } from '../../core/constants';
 import CodeEditor from '../../shared/CodeEditor';
 import './ICode.css';
 import { executeCode } from './ICodeService';
@@ -15,9 +8,9 @@ import { executeCode } from './ICodeService';
 export default function ICode() {
   const [outputEndpoint, setOutputEndpoint] = useState();
 
-  const htmlValueRef = useRef(HTML_SAMPLE);
-  const cssValueRef = useRef(CSS_SAMPLE);
-  const jsValueRef = useRef(JS_SAMPLE);
+  const htmlValueRef = useRef(HTML_CONFIG.sample);
+  const cssValueRef = useRef(CSS_CONFIG.sample);
+  const jsValueRef = useRef(JS_CONFIG.sample);
 
   const createRequestBody = () => ({
     html: htmlValueRef.current,
@@ -35,30 +28,32 @@ export default function ICode() {
     }
   };
 
-  useEffect(() => { handleExecution(); }, []);
+  useEffect(() => {
+    handleExecution();
+  }, []);
 
   return (
     <div className='icode__grid-container px-2'>
       <div className='icode__html'>
         <CodeEditor
-          title={HTML_EDITOR_TITLE}
-          language='html'
+          title={HTML_CONFIG.editorTitle}
+          language={HTML_CONFIG.language}
           valueRef={htmlValueRef}
           onExecution={handleExecution}
         ></CodeEditor>
       </div>
       <div className='icode__css'>
         <CodeEditor
-          title={CSS_EDITOR_TITLE}
-          language='css'
+          title={CSS_CONFIG.editorTitle}
+          language={CSS_CONFIG.language}
           valueRef={cssValueRef}
           onExecution={handleExecution}
         ></CodeEditor>
       </div>
       <div className='icode__js'>
         <CodeEditor
-          title={JS_EDITOR_TITLE}
-          language='javascript'
+          title={JS_CONFIG.editorTitle}
+          language={JS_CONFIG.language}
           valueRef={jsValueRef}
           onExecution={handleExecution}
         ></CodeEditor>
