e707ed11ca61a202df0f12cd002623fbe5f09c30
ciordan <Catalin.Iordan@endava.com>
2020-10-18 21:13:36 +0300

    #42: Refactor

---
+++ src/core/constants.js
@@ -0,0 +1,21 @@
+export const HTML_EDITOR_TITLE = 'HTML';
+export const CSS_EDITOR_TITLE = 'CSS';
+export const JS_EDITOR_TITLE = 'JS';
+
+export const HTML_SAMPLE = `
+<div class='twenty-one-pilots'>Twenty One Pilots</div>
+`;
+
+export const CSS_SAMPLE = `
+.twenty-one-pilots {
+  color: red;
+}
+`;
+
+export const JS_SAMPLE = `
+  function sayHello() {
+    console.log('Hello world');
+  }
+
+  sayHello();
+`;
