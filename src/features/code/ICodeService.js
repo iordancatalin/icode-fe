0f3932dcf901c90eaaecac1a60d582dc3c39abc6
ciordan <Catalin.Iordan@endava.com>
2020-10-20 22:21:54 +0300

    #40: Too many files are being created, optimize the algorithm to reuse some files.

--- src/features/code/ICodeService.js
+++ src/features/code/ICodeService.js
@@ -1,8 +1,17 @@
 export const executeCode = (body) =>
   fetch('http://localhost:8080/api/v1/execute-code', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: createHeadersForExecuteCodeRequest(),
     body: JSON.stringify(body),
   });
+
+const createHeadersForExecuteCodeRequest = () => {
+  const wrkDirectory = sessionStorage.getItem('x-wrk-directory');
+
+  return wrkDirectory
+    ? {
+        'Content-Type': 'application/json',
+        'X-WRK-DIRECTORY': wrkDirectory,
+      }
+    : { 'Content-Type': 'application/json' };
+};
