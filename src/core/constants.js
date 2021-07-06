a9b317124ea5e959f68fb366d83ea1b0ecac319f
ciordan <Catalin.Iordan@endava.com>
2021-07-06 19:12:38 +0300

    Update sample

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,25 +1,54 @@
 export const HTML_CONFIG = {
   editorTitle: 'HTML',
   language: 'html',
-  sample: `<div class='twenty-one-pilots'>Twenty One Pilots</div>`,
+  sample: `
+<div class="container">
+  <h1> Playground area </h1>
+  <button onclick="greet()">Click me</button>
+</div>`,
 };
 
 export const CSS_CONFIG = {
   editorTitle: 'CSS',
   language: 'css',
-  sample: `.twenty-one-pilots {
-    color: red;
+  sample: `
+body {
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: darkcyan;
+}
+
+.container button {
+    padding: 1rem 1.5rem;
+    border: 0;
+    border-radius: 5px;
+    background-color: darkcyan;
+    color: #fff;
+    cursor: pointer;
+    transition: box-shadow .4s ease-in;
+}
+
+.container button:hover {
+    box-shadow: 0 0 7px #000;
 }`,
 };
 
 export const JS_CONFIG = {
   editorTitle: 'JS',
   language: 'javascript',
-  sample: `function sayHello() {
-    console.log('Hello world');
-}
-
-sayHello();`,
+  sample: `
+function greet() {
+  alert('Welcome to playground area! Have fun :)');
+}`,
 };
 
 export const LAYOUT_TYPE_1 = 'layout-1';
